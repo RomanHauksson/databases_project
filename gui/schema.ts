fe3e485a0db5c6ab9d3464b8f41d799e66bc1138
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-11-13 16:52:19 -0600

    Update column names

--- gui/schema.ts
+++ gui/schema.ts
@@ -10,7 +10,7 @@ import {
 } from "drizzle-orm/pg-core";
 
 export const book = pgTable("book", {
-  isbn: char("isbn", { length: 13 }).primaryKey(),
+  isbn13: char("isbn13", { length: 13 }).primaryKey(),
   title: text("title"),
 });
 
@@ -23,14 +23,16 @@ export const bookAuthors = pgTable(
   "book_authors",
   {
     authorId: serial("author_id").references(() => authors.id),
-    isbn: char("isbn", { length: 13 }).references(() => book.isbn),
+    bookIsbn13: char("book_isbn13", { length: 13 }).references(
+      () => book.isbn13
+    ),
   },
-  (table) => [primaryKey({ columns: [table.authorId, table.isbn] })]
+  (table) => [primaryKey({ columns: [table.authorId, table.bookIsbn13] })]
 );
 
 export const borrower = pgTable("borrower", {
   cardId: serial("card_id").primaryKey(),
-  ssn: char("isbn", { length: 9 }),
+  ssn: char("ssn", { length: 9 }),
   name: text("name"),
   address: text("address"),
   phoneNumber: text("phone_number"),
@@ -38,8 +40,8 @@ export const borrower = pgTable("borrower", {
 
 export const bookLoans = pgTable("book_loans", {
   id: serial("id").primaryKey(),
-  isbn: char("isbn", { length: 13 }).references(() => book.isbn),
-  cardId: serial("card_id").references(() => borrower.cardId),
+  bookIsbn13: char("book_isbn13", { length: 13 }).references(() => book.isbn13),
+  borrowerCardId: serial("borrower_card_id").references(() => borrower.cardId),
   address: text("address"),
   phoneNumber: text("phone_number"),
   dateOut: date("date_out"),
@@ -51,6 +53,6 @@ export const fines = pgTable("fines", {
   loanId: serial("loan_id")
     .primaryKey()
     .references(() => bookLoans.id),
-  amount: numeric("amount"),
+  amount: numeric("amount"), // TODO: change to fixed-precision with two decimal places
   paid: boolean(),
 });
