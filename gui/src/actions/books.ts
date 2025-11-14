4abf093abbf1879163437248ac5429ec1d253194
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-11-14 12:13:33 -0600

    Add function signatures for rest of milestone 2

--- gui/src/actions/books.ts
+++ gui/src/actions/books.ts
@@ -1,44 +1,81 @@
 "use server";
 import { db } from "@/db/db";
-import { book, authors, bookAuthors, bookLoans } from "@/db/schema";
+import { book, authors, bookAuthors, bookLoans, borrower } from "@/db/schema";
 import { eq, ilike, sql, or } from "drizzle-orm";
 
-export type BookResult = {
-	title: string | null;
-	isbn13: string;
-	authorNames: string[];
-	isCheckedOut: boolean;
-	numResults: number;
+type Book = typeof book.$inferSelect;
+type Authors = typeof authors.$inferSelect;
+
+export type BookResult = Book & {
+  authorNames: Authors["name"][];
+  isCheckedOut: boolean;
+  numResults: number;
 };
 
 export const searchBooks = async (
-	searchTerm: string,
+  searchTerm: string,
 ): Promise<BookResult[]> => {
-	if (searchTerm === "") {
-		return [];
-	}
+  if (searchTerm === "") {
+    return [];
+  }
 
-	const books = await db
-		.select({
-			isbn13: book.isbn13,
-			title: book.title,
-			authorNames: sql<string[]>`array_agg(${authors.name})`,
-			isCheckedOut: sql<boolean>`BOOL_OR(${bookLoans.bookIsbn13} IS NOT NULL AND ${bookLoans.dateIn} IS NULL)`,
-			numResults: sql<number>`COUNT(*) OVER()`,
-		})
-		.from(book)
-		.leftJoin(bookAuthors, eq(book.isbn13, bookAuthors.bookIsbn13))
-		.leftJoin(authors, eq(bookAuthors.authorId, authors.id))
-		.leftJoin(bookLoans, eq(book.isbn13, bookLoans.bookIsbn13))
-		.where(
-			or(
-				ilike(book.title, `%${searchTerm}%`),
-				ilike(book.isbn13, `%${searchTerm}%`),
-				ilike(authors.name, `%${searchTerm}%`),
-			),
-		)
-		.groupBy(book.isbn13)
-		.limit(50);
+  const books = await db
+    .select({
+      isbn13: book.isbn13,
+      title: book.title,
+      authorNames: sql<string[]>`array_agg(${authors.name})`,
+      isCheckedOut: sql<boolean>`BOOL_OR(${bookLoans.bookIsbn13} IS NOT NULL AND ${bookLoans.dateIn} IS NULL)`,
+      numResults: sql<number>`COUNT(*) OVER()`,
+    })
+    .from(book)
+    .leftJoin(bookAuthors, eq(book.isbn13, bookAuthors.bookIsbn13))
+    .leftJoin(authors, eq(bookAuthors.authorId, authors.id))
+    .leftJoin(bookLoans, eq(book.isbn13, bookLoans.bookIsbn13))
+    .where(
+      or(
+        ilike(book.title, `%${searchTerm}%`),
+        ilike(book.isbn13, `%${searchTerm}%`),
+        ilike(authors.name, `%${searchTerm}%`),
+      ),
+    )
+    .groupBy(book.isbn13)
+    .limit(50);
 
-	return books;
+  return books;
 };
+
+export const checkOut = async (isbn13: string, borrowerCardId: string) => {};
+
+type BookLoan = typeof bookLoans.$inferSelect;
+
+export const searchBookLoans = async (
+  searchTerm: string,
+): Promise<BookLoan[]> => {};
+
+export const checkIn = async (isbn13: string) => {};
+
+type Borrower = typeof borrower.$inferSelect;
+
+export const createBorrower = async ({
+  ssn,
+  name,
+  address,
+  phoneNumber,
+}: Omit<Borrower, "cardId">) => {};
+
+// See Vercel's docs on making cron jobs: https://vercel.com/docs/cron-jobs/quickstart?framework=nextjs-app
+
+export const payBorrowerFines = async (
+  borrowerCardId: Borrower["cardId"],
+) => {};
+
+export const getBorrowerFines = async ({
+  hidePaidFines = true,
+}: {
+  hidePaidFines?: boolean;
+}): Promise<
+  {
+    borrowerCardId: Borrower["cardId"];
+    totalAmount: number;
+  }[]
+> => {};
